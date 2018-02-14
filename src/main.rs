30e08a3c04a0af02b3e840341870c8367b07b8e0
qthree <qthree3@gmail.com>
2018-02-14 21:04:11 +0700

    Yew update fix

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ impl Component<Context> for Model {
     type Msg = Msg;
     type Properties = ();
 
-    fn create(_: &mut Env<Context, Self>) -> Self {
+    fn create(_: (), _: &mut Env<Context, Self>) -> Self {
         Model {
             selected: None
         }
